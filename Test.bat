2923a6d979b961c14e13e28564b6b644d35ea458
ShaikMohamedIrfan <35554090+ShaikMohamedIrfan@users.noreply.github.com>
2019-11-19 12:35:25 +0530

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -1,3 +1,3 @@
 curl http://techteamserver/snap/snaprestapi/UploadTdm/SFandGaTdm -H "Content-type:multipart/form-data" -X POST -F "file=@"C:\Users\Transitus14\Downloads\Salesforce_Mrohini_LeadFunctional_TDM.xlsx"
-curl http://techteamserver/snap/snaprestapi/SfApi/SFCall -H "Content-type:application/json" -X POST -d @"C:\Test.json"
+curl http://techteamserver/snap/snaprestapi/SfApi/SFCall -H "Content-type:application/json" -X POST -d @Test.json
 pause
